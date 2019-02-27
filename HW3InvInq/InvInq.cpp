5553e2cf43fde77d13aa45dc014b85f0e70fa822
Erroll <errollabrahamian@gmail.com>
2019-02-26 23:30:19 -0800

    final v1 0226 23:30

--- HW3InvInq/InvInq.cpp
+++ HW3InvInq/InvInq.cpp
@@ -2,95 +2,29 @@
 // Erroll Abrahamian, CISP 400
 // 03-03-2019
 
-/*
-Table 1 has the minimum fields you will need as well as the validation rules for Specification A2.
-
-             Field Name -- Validation Rules
-       Item Description -- 22 characters max
-       Quantity at Hand -- Cannot be negative
-            Retail Cost -- Automatically calculate 100% markup
-Date Added to Inventory -- System should enter this
-
-This is a menu driven program. You can decide if it uses a numeric or alphabetic menu. Keep prompting and running until the quit option is selected.
-• Style guide elements apply: comments, layout, Program Greeting, Source File Header, and variables etc. etc.
-
-// Specification C1 - Dynamic Array
-// Create an array on the heap. Increase it automatically for every item the client adds. That is, this array should be as small as possible - always.
-
-// Specification C2 - Add Option
-// Allows you to add records to the inventory data structure you created.
-
-// Specification C3 - Display Option
-// Displays all the information in your inventory data structure.
-
-// Specification C4 - Main Menu
-// Create a main menu which lists the allowable behaviors. See figure 1 for an example menu.
-
-1. Add Inventory
-2. Display Inventory
-3. Quit Program
-
-// Specification C5 - Validate Menu
-// Validation - Notice allowable options and if entry is incorrect.
-
-// Specification B1 - OOP
-// Code this assignment using at least one class. Put this comment above the class declaration.
-
-// Specification B2 - Delete Inventory Item
-// Allow the client to delete items in the inventory data structure. See figure 2 for the "B" menu. Don’t forget to update C5 to account for this new menu option.
-
-1. Add Inventory
-2. Delete Inventory
-3. Display Inventory
-4. Quit Program
-
-// Specification B3 - Shrink Dynamic Array
-// Resize the dynamic array when you delete inventory.
-
-// Specification A1 - Edit Inventory
-// Add the capability to edit the contents of any inventory item. Edit is different from delete. See figure 3 for the "A" menu. Don’t forget to update C5 to account for this new menu option.
-
-1. Add Inventory
-2. Delete Inventory
-3. Edit Inventory
-4. Display Inventory
-5. Quit Program
-
-// Specification A2 - Validate Inventory
-// Only allow data which meets the input specifications in the inventory data structure.
-
-// Specification A3 - Logfile
-// Log all changes to the inventory data structure to a text file for archival purposes.
-*/
-
 #include <iostream>
 #include <iomanip>
 #include <fstream>
 using namespace std;
 
-/*
-Field Name -- Validation Rules
-Item Description -- 22 characters max
-Quantity at Hand -- Cannot be negative
-Retail Cost -- Automatically calculate 100% markup
-Date Added to Inventory -- System should enter this
-*/
-
 struct Inventory
 {
   string itemDesc;
-  int itemQuan;
-  float itemCost;
+  double itemQuan;
+  double itemDollars;
+  int itemCents;
   int itemDate[3];
 };
 
+// Specification B1 - OOP
 class InvInq
 {
 private:
   Inventory* invArray = nullptr;
-  int itemCount;
+  int itemCount, currInvItem;
+  const int maxLength = 22;
 public:
-  void buildArray();
+  void newArray();
   void printMenu();
   void addInv();
   void incInv();
@@ -98,22 +32,25 @@ public:
   void editInv();
   void showInv();
   void getDate();
-  void delArray();
+  void writeInv();
 };
 
 int main()
 {
   InvInq hw3;
 
-  hw3.buildArray();
-
+  hw3.newArray();
   hw3.printMenu();
+
+  return 0;
 }
 
-void InvInq::buildArray()
+void InvInq::newArray()
 {
+  // number of inventoried items = 0
   itemCount = 0;
-  invArray = new Inventory[1];
+  // Specification C1 - Dynamic Array
+  invArray = new Inventory[0];
 }
 
 void InvInq::printMenu()
@@ -122,8 +59,9 @@ void InvInq::printMenu()
 
   do {
     cout << endl;
-    cout << string(50, '*');
+    cout << string(40, '*');
     cout << endl;
+    // Specification C4 - Main Menu
     cout << "1. Add Inventory\n";
     cout << "2. Delete Inventory\n";
     cout << "3. Edit Inventory\n";
@@ -133,6 +71,7 @@ void InvInq::printMenu()
     cout << "Enter the number for your selection: ";
     cin >> menu;
 
+    // Specification C5 - Validate Menu
     switch (menu)
     {
       case 1: addInv();
@@ -149,36 +88,60 @@ void InvInq::printMenu()
   } while(menu != 5);
 }
 
+// Specification C2 - Add Option
 void InvInq::addInv()
 {
+  incInv();
+
+  // current inventory item is 1 less than the number of items
+  // when number of items = 1, array location must = 0, etc
+  currInvItem = itemCount - 1;
+
   cin.ignore();
   cout << "\nDescription: ";
+
   do {
-    getline(cin, invArray[itemCount].itemDesc);
-    if ((invArray[itemCount].itemDesc).length() > 22)
+    getline(cin, invArray[currInvItem].itemDesc);
+    // Specification A2 - Validate Inventory
+    if ((invArray[currInvItem].itemDesc).length() > maxLength)
       cout << "Description is too long. Enter 22 characters or less:\n";
-  } while(invArray[itemCount].itemDesc.length() > 22);
+  } while(invArray[currInvItem].itemDesc.length() > maxLength);
 
   cout << "Quantity: ";
   do {
-    cin >> invArray[itemCount].itemQuan;
-    if (invArray[itemCount].itemQuan < 0 || invArray[itemCount].itemQuan > 9999999999)
+    cin >> invArray[currInvItem].itemQuan;
+    // allowing ten digits for formatting quantity, so: no more than 9,999,999,999
+    if (invArray[currInvItem].itemQuan < 0 || invArray[currInvItem].itemQuan > 9999999999)
       cout << "That doesn't sound like a proper quantity. Enter a proper amount: ";
-  } while(invArray[itemCount].itemQuan < 0 || invArray[itemCount].itemQuan > 9999999999);
+  } while(invArray[currInvItem].itemQuan < 0 || invArray[currInvItem].itemQuan > 9999999999);
 
-  cout << "Cost: ";
-  cin >> invArray[itemCount].itemCost;
+  cout << "Cost, dollars: ";
+  do {
+    cin >> invArray[currInvItem].itemDollars;
+    // allowing nine digits for formatting dollars, so: no more than 999,999,999
+    if (invArray[currInvItem].itemDollars < 0 || invArray[currInvItem].itemDollars > 999999999)
+      cout << "Dollars should be at least 0 or less than one billion: ";
+  } while(invArray[currInvItem].itemDollars < 0 || invArray[currInvItem].itemDollars > 999999999);
+
+  cout << "Cost, cents: ";
+  do {
+    cin >> invArray[currInvItem].itemCents;
+    if (invArray[currInvItem].itemCents < 0 || invArray[currInvItem].itemCents > 99)
+      cout << "Cents should be between 0 and 99: ";
+  } while(invArray[currInvItem].itemCents < 0 || invArray[currInvItem].itemCents > 99);
 
   getDate();
 }
 
 void InvInq::incInv()
 {
+  // increase number of items by 1
   itemCount++;
 
-  int* tmpArr = nullptr;
+  Inventory* tmpArr = nullptr;
 
-  tmpArr = new int[itemCount];
+  // set temp array to new size of items
+  tmpArr = new Inventory[itemCount];
 
   for (int i = 0; i < (itemCount - 1); i++)
     tmpArr[i] = invArray[i];
@@ -190,23 +153,130 @@ void InvInq::incInv()
   tmpArr = nullptr;
 }
 
+// Specification B2 - Delete Inventory Item
 void InvInq::delInv()
 {
+  bool correctItem = false;
+  int itemNum;
+  char menu;
 
+  do {
+    cout << "\nEnter the Item # of the inventory item you would like to delete,\n";
+    cout << "or enter 0 to return to the main menu: ";
+    cin >> itemNum;
+    if (itemNum == 0)
+      break;
+    if (itemNum > itemCount)
+      cout << "\nOops, we don't have that many items!";
+    else
+    {
+      cout << "\nItem #" << itemNum << " is " << invArray[itemNum - 1].itemDesc << ".\n";
+      cout << "Is this the item you would like to delete?\n";
+      cout << "(y), otherwise enter any other key: ";
+      cin >> menu;
+      if (toupper(menu) == 'Y')
+        correctItem = true;
+    }
+  } while(!correctItem);
+
+  // Specification B3 - Shrink Dynamic Array
+  if (correctItem)
+  {
+    int j = 0;
+    // the element to delete is offset by -1 to align with array
+    int deleteEle = itemNum - 1;
+
+    itemCount--;
+
+    Inventory* delArr = nullptr;
+
+    delArr = new Inventory[itemCount];
+
+    for (int i = 0; i < (itemCount + 1); i++)
+    {
+      if (i != deleteEle)
+        delArr[j] = invArray[i];
+        j++;
+    }
+
+    delete [] invArray;
+
+    invArray = delArr;
+
+    delArr = nullptr;
+  }
 }
 
+// Specification A1 - Edit Inventory
 void InvInq::editInv()
 {
+  bool correctItem = false;
+  int itemNum;
+  char menu;
 
+  do {
+    cout << "\nEnter the Item # of the inventory item you would like to edit,\n";
+    cout << "or enter 0 to return to the main menu: ";
+    cin >> itemNum;
+    if (itemNum == 0)
+      break;
+    if (itemNum > itemCount)
+      cout << "\nOops, we don't have that many items!";
+    else
+    {
+      cout << "\nItem #" << itemNum << " is " << invArray[itemNum - 1].itemDesc << ".\n";
+      cout << "Is this the item you would like to edit?\n";
+      cout << "(y), otherwise enter any other key: ";
+      cin >> menu;
+      if (toupper(menu) == 'Y')
+        correctItem = true;
+    }
+  } while(!correctItem);
+
+  if (correctItem)
+  {
+    cout << "Old Description: " << invArray[itemNum - 1].itemDesc << ".\n";
+    cout << "New Description: ";
+    cin.ignore();
+    do {
+      getline(cin, invArray[itemNum - 1].itemDesc);
+      if ((invArray[itemNum - 1].itemDesc).length() > maxLength)
+        cout << "Description is too long. Enter 22 characters or less:\n";
+    } while(invArray[itemNum - 1].itemDesc.length() > maxLength);
+
+    cout << "Previous Quantity: " << invArray[itemNum - 1].itemQuan << ".\n";
+    cout << "New Quantity: ";
+    do {
+      cin >> invArray[itemNum - 1].itemQuan;
+      if (invArray[itemNum - 1].itemQuan < 0 || invArray[itemNum - 1].itemQuan > 9999999999)
+        cout << "That doesn't sound like a proper quantity. Enter a proper amount: ";
+    } while(invArray[itemNum - 1].itemQuan < 0 || invArray[itemNum - 1].itemQuan > 9999999999);
+
+    cout << "Previous Cost: $" << invArray[itemNum - 1].itemDollars << "." << invArray[itemNum - 1].itemCents << ".\n";
+    cout << "New Cost, dollars: ";
+    do {
+      cin >> invArray[itemNum - 1].itemDollars;
+      if (invArray[itemNum - 1].itemDollars < 0 || invArray[itemNum - 1].itemDollars > 999999999)
+        cout << "Dollars should be at least 0 or less than one billion: ";
+    } while(invArray[itemNum - 1].itemDollars < 0 || invArray[itemNum - 1].itemDollars > 999999999);
+
+    cout << "New Cost, cents: ";
+    do {
+      cin >> invArray[currInvItem].itemCents;
+      if (invArray[currInvItem].itemCents < 0 || invArray[currInvItem].itemCents > 99)
+        cout << "Cents should be between 0 and 99: ";
+    } while(invArray[currInvItem].itemCents < 0 || invArray[currInvItem].itemCents > 99);
+  }
 }
 
+// Specification C3 - Display Option
 void InvInq::showInv()
 {
-  cout << "\nDESCRIPTION             --    QUANTITY  --          COST --   ENTRY DATE\n";
-  //      "1234567890123456789012      1234567890      123456789.01      12/31/1983"
+  cout << "\nITEM #  DESCRIPTION                   QUANTITY              COST      ENTRY DATE\n";
+  //      "        1234567890123456789012      1234567890        123456789.01      12/31/1983"
   for (int i = 0; i < itemCount; i++)
   {
-    cout << left << setw(22) << invArray[i].itemDesc << "      " << right << setw(10) << invArray[i].itemQuan << "      " << setw(12) << fixed << setprecision(2) << invArray[i].itemCost << "      " << setw(2) << invArray[i].itemDate[0] << "/" << setw(2) << invArray[i].itemDate[1] << "/" << invArray[i].itemDate[2] << endl;
+    cout << right << setw(6) << i + 1 << "  " << left << setw(22) << invArray[i].itemDesc << "      " << right << setw(10) << invArray[i].itemQuan << "      " << setw(9) << invArray[i].itemDollars << "." << setw(2) << invArray[i].itemCents << "      " << setw(2) << invArray[i].itemDate[0] << "/" << setw(2) << invArray[i].itemDate[1] << "/" << invArray[i].itemDate[2] << endl;
   }
 }
 
@@ -217,16 +287,22 @@ void InvInq::getDate()
   int dtYear = (1900 + dateTime->tm_year);
   int dtMonth = (1 + dateTime->tm_mon);
   int dtDay = dateTime->tm_mday;
-  /*
-  cout << "MONTH  DAY  YEAR" << endl;
-  cout << dtMonth << dtDay << dtYear << endl;
-  */
-  invArray[itemCount].itemDate[0] = dtMonth;
-  invArray[itemCount].itemDate[1] = dtDay;
-  invArray[itemCount].itemDate[2] = dtYear;
+
+  invArray[currInvItem].itemDate[0] = dtMonth;
+  invArray[currInvItem].itemDate[1] = dtDay;
+  invArray[currInvItem].itemDate[2] = dtYear;
 }
 
-void InvInq::delArray()
+// Specification A3 - Logfile
+void InvInq::writeInv()
 {
+  ofstream invFile;
+  invFile.open("inv.txt", ios::app);
 
+  invFile << "\nITEM #  DESCRIPTION                   QUANTITY              COST      ENTRY DATE\n";
+
+  for (int i = 0; i < itemCount; i++)
+  {
+    invFile << right << setw(6) << i + 1 << "  " << left << setw(22) << invArray[i].itemDesc << "      " << right << setw(10) << invArray[i].itemQuan << "      " << setw(9) << invArray[i].itemDollars << "." << setw(2) << invArray[i].itemCents << "      " << setw(2) << invArray[i].itemDate[0] << "/" << setw(2) << invArray[i].itemDate[1] << "/" << invArray[i].itemDate[2] << endl;
+  }
 }
